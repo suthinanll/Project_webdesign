11e72ab44c877aed7cfc33197f4d57201f5ceaaa
Ptong <suthinan.l@kkumail.com>
2024-09-11 21:23:46 +0700

    หน้าล็อกอิน สมัคร ลืมรหัสผ่าน ที่อยู่รวม เพิ่มที่อยู่ ตั้งรหัสผ่านใหม่

---
+++ js/scrip.js
@@ -0,0 +1,16 @@
+function register() {
+    alert("สมัครสมาชิกเรียบร้อย");
+    
+    window.location.href = "login.html";
+}
+function nextpage() {
+    window.location.href = "newpassword.html";
+}
+function newpassword() {
+    alert("ตั้งค่ารหัสผ่านใหม่เรียบร้อย");
+    
+    window.location.href = "login.html";
+}
+function address() {    
+    window.location.href = "myadd.html";
+}
